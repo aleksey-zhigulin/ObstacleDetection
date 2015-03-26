890d6e4d2ef5ba861ca017dcbc97155e783d2266
aleksey-zhigulin <a.a.zhigulin@yandex.ru>
2015-03-26 14:34:31 +0300

    Init project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Aleksey
+ *
+ * Created on March 26, 2015, 2:31 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
